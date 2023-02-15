d4e7a0a101c30c7d5b56029cc4843f1d4165eba5
Aku <micedreams@micedreams.domain.name>
2023-02-15 22:10:15 +0530

    Add update method.

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -32,123 +32,151 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ValueListenableBuilder(
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: ValueListenableBuilder(
             valueListenable: box.listenable(),
             builder: (context, box, _) {
               if (box.isEmpty) {
-                return const Center(
-                  child: Text('Add a contact'),
-                );
+                return const Center(child: Text('Add a contact'));
               } else {
                 return ListView.separated(
                   itemCount: box.length,
-                  itemBuilder: (context, i) {
-                    return ListTile(
-                        onTap: () => _showDetails(context, box, i),
-                        title: Text(box.getAt(i).name),
-                        subtitle: Text(box.getAt(i).country),
-                        trailing: IconButton(
-                          onPressed: () => _deleteInfo(i),
-                          icon: const Icon(
-                            Icons.delete,
-                          ),
-                        ));
-                  },
-                  separatorBuilder: (context, i) => const Divider(),
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) => ListTile(
+                    onLongPress: () => updateDetails(context, box, index),
+                    onTap: () => showDetails(context, box, index),
+                    title: Text(box.getAt(index).name),
+                    subtitle: Text(box.getAt(index).country),
+                    trailing: IconButton(
+                      onPressed: () => deletePeople(index),
+                      icon: const Icon(Icons.delete),
+                    ),
+                  ),
                 );
               }
-            }),
-        floatingActionButton: FloatingActionButton(
-            onPressed: _addInfo, child: const Icon(Icons.add)),
+            },
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: addDetails,
+            child: const Icon(Icons.add),
+          ),
+        ),
+      );
+
+  void showDetails(BuildContext context, Box<dynamic> box, int index) =>
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Person Details'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(box.getAt(index).name),
+              Text(box.getAt(index).country),
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      );
+
+  void addDetails() async => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Add Person'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: nameController),
+              TextField(controller: countryController),
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('OK'),
+              onPressed: () {
+                addPeople(
+                  nameController.text.trim(),
+                  countryController.text.trim(),
+                );
+                nameController.clear();
+                countryController.clear();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+
+  void updateDetails(BuildContext context, Box box, int index) {
+    nameController.text = box.getAt(index).name;
+    countryController.text = box.getAt(index).country;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Add Person'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(controller: nameController),
+            TextField(controller: countryController),
+          ],
+        ),
+        actions: <Widget>[
+          ElevatedButton(
+            child: const Text('OK'),
+            onPressed: () {
+              updatePeople(
+                nameController.text.trim(),
+                countryController.text.trim(),
+                box,
+                index,
+              );
+              nameController.clear();
+              countryController.clear();
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
 
-  Future<dynamic> _showDetails(BuildContext context, Box<dynamic> box, int i) {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Person Details'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(box.getAt(i).name),
-                Text(box.getAt(i).country),
-              ],
-            ),
-            actions: <Widget>[
-              ElevatedButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        });
-  }
-
-  _addInfo() async {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Add Person'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(controller: nameController),
-                TextField(controller: countryController),
-              ],
-            ),
-            actions: <Widget>[
-              ElevatedButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  addPeople(
-                    nameController.text.trim(),
-                    countryController.text.trim(),
-                  );
-                  nameController.clear();
-                  countryController.clear();
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        });
-  }
-
-  void addPeople(name, country) {
-    People newPerson = People(
-      name: nameController.text,
-      country: countryController.text,
+  void addPeople(String name, String country) {
+    final newPerson = People(
+      name: name,
+      country: country,
     );
+
     if (!newPerson.existsInList(box.values)) {
       box.add(newPerson);
     }
   }
 
+  void updatePeople(String name, String country, Box box, int index) {
+    final newPerson = People(
+      name: name,
+      country: country,
+    );
 
-  _updateInfo() {
-    // final peopleList = <People>[];
-    // peopleList.add(People(name: 'aku', country: 'India'));
-    // box.put('peopleList', peopleList);
-    // peopleListNotifier.value = peopleList;
+    if (!newPerson.existsInList(box.values)) {
+      box.putAt(index, newPerson);
+    }
   }
 
-  _deleteInfo(int index) {
+  void deletePeople(int index) {
     box.deleteAt(index);
     box.delete("$index");
-    print('Info deleted from box!');
   }
 }
